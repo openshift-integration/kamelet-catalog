91090b362597b712bd92dcddf3b6d8666d4c590c
Lucie Krejcirova <lfabriko@notebook-lucka-prace-nej.lan>
2020-12-17 18:57:06 +0100

    ENTESB-15380 Jira-source yaks test

---
+++ test/jira/jira-source.feature
@@ -0,0 +1,35 @@
+Feature: Jira Kamelet
+
+  Background:
+    Given Disable auto removal of Kamelet resources
+
+  Scenario: Verify resources
+    Given Kamelet jira-source is available
+    Given Kamelet logger-sink is available
+    Given KameletBinding inmem-to-log is available
+    Given Camel-K integration jira-to-inmem is running
+
+  Scenario: Verify new jira issue is created
+    Given variable summary is "New bug, citrus:randomString(10)"
+    Given URL: ${url}
+    And HTTP request header Authorization="Basic ${base64encoded}"
+    And HTTP request header Content-Type="application/json"
+    And HTTP request body
+    """
+    {
+         "fields": {
+               "project":
+               {
+                  "key": "${project}"
+               },
+               "summary": "${summary}",
+               "description": "Yaks test of jira-source kamelet",
+               "issuetype": {
+                  "id": "1"
+               }
+           }
+    }
+    """
+    When send POST /rest/api/2/issue/
+    Then receive HTTP 201 CREATED
+    Then Camel-K integration inmem-to-log should print "${summary}"
---
+++ test/jira/prepare-structure.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# create secret from properties file
+kubectl create secret generic conn-credentials --from-file=conn-credentials.properties -n ${YAKS_NAMESPACE}
+
+# bind secret to jira-source test
+kubectl label secret conn-credentials yaks.citrusframework.org/test=jira-source -n ${YAKS_NAMESPACE}
+
+# create InMemoryChannel messages
+oc apply -f inmem.yaml -n ${YAKS_NAMESPACE}
+
+# create logger-sink Kamelet
+oc apply -f logger-sink.kamelet.yaml -n ${YAKS_NAMESPACE}
+
+# create KameletBinding inmem-to-log from InMemoryChannel messages to logger-sink Kamelet
+oc apply -f inmem-to-log.binding.yaml -n ${YAKS_NAMESPACE}
+
+# create integration jira-source kamelet to InMemoryChannel messages, with property based configuration
+kamel run jira-to-inmem.groovy --property-file=jira-credentials.properties -w -n ${YAKS_NAMESPACE}
