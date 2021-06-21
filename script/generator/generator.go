74823250862ef1130dde176d4387c37f6c66049e
Nicola Ferraro <ni.ferraro@gmail.com>
2021-06-21 16:22:17 +0200

    Use template for doc and customize for the product (#99)

--- script/generator/generator.go
+++ script/generator/generator.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
 	"sort"
 	"strings"
+	"text/template"
 
 	camel "github.com/apache/camel-k/pkg/apis/camel/v1alpha1"
+	"github.com/iancoleman/strcase"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
@@ -25,17 +30,256 @@ func main() {
 	dir := os.Args[1]
 	out := os.Args[2]
 
+	funcMap := template.FuncMap{
+		"ToCamel": strcase.ToCamel,
+	}
+
+	templateFile := path.Join(out, "kamelet.adoc.tmpl")
+	t, err := template.New("kamelet.adoc.tmpl").Funcs(funcMap).ParseFiles(templateFile)
+	handleGeneralError(fmt.Sprintf("cannot load template file from %s", templateFile), err)
+
 	kamelets := listKamelets(dir)
 
 	links := make([]string, 0)
 	for _, k := range kamelets {
 		img := saveImage(k, out)
-		produceDoc(k, out, img)
+
+		ctx := NewTemplateContext(k, img)
+
+		buffer := new(bytes.Buffer)
+		err = t.Execute(buffer, &ctx)
+		handleGeneralError("cannot process template", err)
+
+		produceDocFile(k, out, buffer.String())
 
 		links = append(links, fmt.Sprintf("* xref:ROOT:%s.adoc[%s %s]", k.Name, img, k.Spec.Definition.Title))
 	}
 
 	saveNav(links, out)
+}
+
+type TemplateContext struct {
+	Kamelet camel.Kamelet
+	Image   string
+}
+
+func NewTemplateContext(kamelet camel.Kamelet, image string) TemplateContext {
+	return TemplateContext{
+		Kamelet: kamelet,
+		Image:   image,
+	}
+}
+
+type Prop struct {
+	Name     string
+	Title    string
+	Required bool
+	Default  *string
+	Example  *string
+}
+
+func (p Prop) GetSampleValue() string {
+	if p.Default != nil {
+		return *p.Default
+	}
+	if p.Example != nil {
+		return *p.Example
+	}
+	return fmt.Sprintf(`"The %s"`, p.Title)
+}
+
+func getSortedProps(k camel.Kamelet) []Prop {
+	required := make(map[string]bool)
+	props := make([]Prop, 0, len(k.Spec.Definition.Properties))
+	for _, r := range k.Spec.Definition.Required {
+		required[r] = true
+	}
+	for key := range k.Spec.Definition.Properties {
+		prop := k.Spec.Definition.Properties[key]
+		var def *string
+		if prop.Default != nil {
+			b, err := prop.Default.MarshalJSON()
+			handleGeneralError(fmt.Sprintf("cannot marshal property %q default value in Kamelet %s", key, k.Name), err)
+			defVal := string(b)
+			def = &defVal
+		}
+		var ex *string
+		if prop.Example != nil {
+			b, err := prop.Example.MarshalJSON()
+			handleGeneralError(fmt.Sprintf("cannot marshal property %q example value in Kamelet %s", key, k.Name), err)
+			exVal := string(b)
+			ex = &exVal
+		}
+		props = append(props, Prop{Name: key, Title: prop.Title, Required: required[key], Default: def, Example: ex})
+	}
+	sort.Slice(props, func(i, j int) bool {
+		ri := props[i].Required
+		rj := props[j].Required
+		if ri && !rj {
+			return true
+		} else if !ri && rj {
+			return false
+		}
+		return props[i].Name < props[j].Name
+	})
+	return props
+}
+
+func (ctx *TemplateContext) HasProperties() bool {
+	return len(ctx.Kamelet.Spec.Definition.Properties) > 0
+}
+
+func (ctx *TemplateContext) Properties() string {
+	content := ""
+	if len(ctx.Kamelet.Spec.Definition.Properties) > 0 {
+		props := getSortedProps(ctx.Kamelet)
+		content += `[width="100%",cols="2,^2,3,^2,^2,^3",options="header"]` + "\n"
+		content += "|===\n"
+		content += tableLine("Property", "Name", "Description", "Type", "Default", "Example")
+
+		for _, propDef := range props {
+			key := propDef.Name
+			prop := ctx.Kamelet.Spec.Definition.Properties[key]
+			name := key
+			if propDef.Required {
+				name = "*" + name + " {empty}* *"
+			}
+			var def string
+			if propDef.Default != nil {
+				def = "`" + strings.ReplaceAll(*propDef.Default, "`", "'") + "`"
+			}
+			var ex string
+			if propDef.Example != nil {
+				ex = "`" + strings.ReplaceAll(*propDef.Example, "`", "'") + "`"
+			}
+			content += tableLine(name, prop.Title, prop.Description, prop.Type, def, ex)
+		}
+
+		content += "|===\n"
+
+	}
+	return content
+}
+
+func (ctx *TemplateContext) ExampleBinding(apiVersion, kind, name string) string {
+	content := ""
+	propDefs := getSortedProps(ctx.Kamelet)
+	tp := ctx.Kamelet.ObjectMeta.Labels["camel.apache.org/kamelet.type"]
+	if tp != "" {
+		sampleConfig := make([]string, 0)
+		for _, propDef := range propDefs {
+			if !propDef.Required {
+				continue
+			}
+			key := propDef.Name
+			if propDef.Default == nil {
+				ex := propDef.GetSampleValue()
+				sampleConfig = append(sampleConfig, fmt.Sprintf("%s: %s", key, ex))
+			}
+		}
+		props := ""
+		if len(sampleConfig) > 0 {
+			props += "    properties:\n"
+			for _, p := range sampleConfig {
+				props += fmt.Sprintf("      %s\n", p)
+			}
+		}
+
+		kameletRef := fmt.Sprintf(`    ref:
+      kind: Kamelet
+      apiVersion: camel.apache.org/v1alpha1
+      name: %s
+%s`, ctx.Kamelet.Name, props)
+
+		boundToRef := fmt.Sprintf(`    ref:
+      kind: %s
+      apiVersion: %s
+      name: %s
+`, kind, apiVersion, name)
+		var sourceRef string
+		var sinkRef string
+		var steps string
+
+		switch tp {
+		case "source":
+			sourceRef = kameletRef
+			sinkRef = boundToRef
+		case "sink":
+			sourceRef = boundToRef
+			sinkRef = kameletRef
+		case "action":
+			sourceRef = `    ref:
+      kind: Kamelet
+      apiVersion: camel.apache.org/v1alpha1
+      name: timer-source
+    properties:
+      message: "Hello"`
+			sinkRef = boundToRef
+			steps = fmt.Sprintf(`
+  steps:
+  -%s`, kameletRef[3:])
+		}
+
+		binding := fmt.Sprintf(`apiVersion: camel.apache.org/v1alpha1
+kind: KameletBinding
+metadata:
+  name: %s-binding
+spec:
+  source:
+%s%s  sink:
+%s
+`, ctx.Kamelet.Name, sourceRef, steps, sinkRef)
+
+		content += fmt.Sprintf(".%s-binding.yaml\n", ctx.Kamelet.Name)
+		content += "[source,yaml]\n"
+		content += "----\n"
+		content += binding
+		content += "----\n"
+
+	}
+	return content
+}
+
+func (ctx *TemplateContext) ExampleKamelBindCommand(ref string) string {
+	tp := ctx.Kamelet.ObjectMeta.Labels["camel.apache.org/kamelet.type"]
+	var prefix string
+	switch tp {
+	case "source":
+		prefix = "source."
+	case "sink":
+		prefix = "sink."
+	case "action":
+		prefix = "step-0."
+	default:
+		handleGeneralError("unknown kamelet type", errors.New(tp))
+	}
+
+	cmd := "kamel bind "
+	timer := "timer-source?message=Hello"
+	kamelet := ctx.Kamelet.Name
+	propDefs := getSortedProps(ctx.Kamelet)
+	for _, p := range propDefs {
+		if p.Required && p.Default == nil {
+			val := p.GetSampleValue()
+			if strings.HasPrefix(val, `"`) {
+				kamelet += fmt.Sprintf(` -p "%s%s=%s`, prefix, p.Name, val[1:])
+			} else {
+				kamelet += fmt.Sprintf(" -p %s%s=%s", prefix, p.Name, val)
+			}
+		}
+	}
+
+	switch tp {
+	case "source":
+		return cmd + kamelet + " " + ref
+	case "sink":
+		return cmd + ref + " " + kamelet
+	case "action":
+		return cmd + timer + " --step " + kamelet + " " + ref
+	default:
+		handleGeneralError("unknown kamelet type", errors.New(tp))
+	}
+	return ""
 }
 
 func saveNav(links []string, out string) {
@@ -76,194 +320,8 @@ func saveImage(k camel.Kamelet, out string) string {
 	return ""
 }
 
-func produceDoc(k camel.Kamelet, out string, image string) {
+func produceDocFile(k camel.Kamelet, out string, content string) {
 	docFile := filepath.Join(out, "pages", k.Name+".adoc")
-
-	content := "// THIS FILE IS AUTOMATICALLY GENERATED: DO NOT EDIT\n"
-	content += "= " + image + " " + k.Spec.Definition.Title + "\n"
-	content += "\n"
-	if prov, ok := k.Annotations["camel.apache.org/provider"]; ok {
-		content += fmt.Sprintf("*Provided by: %q*\n", prov)
-		content += "\n"
-	}
-	if supportLevel, ok := k.Annotations["camel.apache.org/kamelet.support.level"]; ok {
-		content += fmt.Sprintf("*Support Level for this Kamelet is: %q*\n", supportLevel)
-		content += "\n"
-	}
-	content += k.Spec.Definition.Description + "\n"
-	content += "\n"
-	content += "== Configuration Options\n"
-	content += "\n"
-
-	required := make(map[string]bool)
-	keys := make([]string, 0, len(k.Spec.Definition.Properties))
-
-	if len(k.Spec.Definition.Properties) > 0 {
-		for _, r := range k.Spec.Definition.Required {
-			required[r] = true
-		}
-
-		for key := range k.Spec.Definition.Properties {
-			keys = append(keys, key)
-		}
-		sort.Slice(keys, func(i, j int) bool {
-			ri := required[keys[i]]
-			rj := required[keys[j]]
-			if ri && !rj {
-				return true
-			} else if !ri && rj {
-				return false
-			}
-			return keys[i] < keys[j]
-		})
-
-		content += fmt.Sprintf("The following table summarizes the configuration options available for the `%s` Kamelet:\n", k.Name)
-
-		content += `[width="100%",cols="2,^2,3,^2,^2,^3",options="header"]` + "\n"
-		content += "|===\n"
-		content += tableLine("Property", "Name", "Description", "Type", "Default", "Example")
-
-		for _, key := range keys {
-			prop := k.Spec.Definition.Properties[key]
-			name := key
-			if required[key] {
-				name = "*" + name + " {empty}* *"
-			}
-			def := ""
-			if prop.Default != nil {
-				b, err := prop.Default.MarshalJSON()
-				handleGeneralError(fmt.Sprintf("cannot marshal property %q default value in Kamelet %s", key, k.Name), err)
-				def = "`" + strings.ReplaceAll(string(b), "`", "'") + "`"
-			}
-			ex := ""
-			if prop.Example != nil {
-				b, err := prop.Example.MarshalJSON()
-				handleGeneralError(fmt.Sprintf("cannot marshal property %q example value in Kamelet %s", key, k.Name), err)
-				ex = "`" + strings.ReplaceAll(string(b), "`", "'") + "`"
-			}
-			content += tableLine(name, prop.Title, prop.Description, prop.Type, def, ex)
-		}
-
-		content += "|===\n"
-		content += "\n"
-		content += "NOTE: Fields marked with ({empty}*) are mandatory.\n"
-
-	} else {
-		content += "The Kamelet does not specify any configuration option.\n"
-	}
-
-	content += "\n"
-	content += "== Usage\n"
-	content += "\n"
-	content += fmt.Sprintf("This section summarizes how the `%s` can be used in various contexts.\n", k.Name)
-	content += "\n"
-
-	tp := k.ObjectMeta.Labels["camel.apache.org/kamelet.type"]
-	if tp != "" {
-		content += fmt.Sprintf("=== Knative %s\n", strings.Title(tp))
-		content += "\n"
-
-		if tp != "action" {
-			content += fmt.Sprintf("The `%s` Kamelet can be used as Knative %s by binding it to a Knative object.\n", k.Name, tp)
-		} else {
-			content += fmt.Sprintf("The `%s` Kamelet can be used as intermediate step in a binding.\n", k.Name)
-		}
-		content += "\n"
-
-		sampleConfig := make([]string, 0)
-		for _, key := range keys {
-			if !required[key] {
-				continue
-			}
-			prop := k.Spec.Definition.Properties[key]
-			if prop.Default == nil {
-				ex := ""
-				if prop.Example != nil {
-					b, err := prop.Example.MarshalJSON()
-					handleGeneralError(fmt.Sprintf("cannot marshal property %q example value in Kamelet %s", key, k.Name), err)
-					ex = string(b)
-				}
-				if ex == "" {
-					ex = `"The ` + prop.Title + `"`
-				}
-				sampleConfig = append(sampleConfig, fmt.Sprintf("%s: %s", key, ex))
-			}
-		}
-		props := ""
-		if len(sampleConfig) > 0 {
-			props += "    properties:\n"
-			for _, p := range sampleConfig {
-				props += fmt.Sprintf("      %s\n", p)
-			}
-		}
-
-		kameletRef := fmt.Sprintf(`    ref:
-      kind: Kamelet
-      apiVersion: camel.apache.org/v1alpha1
-      name: %s
-%s`, k.Name, props)
-
-		knativeRef := `    ref:
-      kind: InMemoryChannel
-      apiVersion: messaging.knative.dev/v1
-      name: mychannel
-`
-		var sourceRef string
-		var sinkRef string
-		var steps string
-
-		switch tp {
-		case "source":
-			sourceRef = kameletRef
-			sinkRef = knativeRef
-		case "sink":
-			sourceRef = knativeRef
-			sinkRef = kameletRef
-		case "action":
-			sourceRef = `    ref:
-      kind: Kamelet
-      apiVersion: camel.apache.org/v1alpha1
-      name: timer-source
-    properties:
-      message: "Hello"`
-			sinkRef = knativeRef
-			steps = fmt.Sprintf(`
-  steps:
-  -%s`, kameletRef[3:])
-		}
-
-		binding := fmt.Sprintf(`apiVersion: camel.apache.org/v1alpha1
-kind: KameletBinding
-metadata:
-  name: %s-binding
-spec:
-  source:
-%s%s  sink:
-%s
-`, k.Name, sourceRef, steps, sinkRef)
-
-		content += fmt.Sprintf(".%s-binding.yaml\n", k.Name)
-		content += "[source,yaml]\n"
-		content += "----\n"
-		content += binding
-		content += "----\n"
-
-		content += "\n"
-		content += "Make sure you have Camel K installed into the Kubernetes cluster you're connected to.\n"
-		content += "\n"
-		content += fmt.Sprintf("Save the `%s-binding.yaml` file into your hard drive, then configure it according to your needs.\n", k.Name)
-		content += "\n"
-		content += fmt.Sprintf("You can run the %s using the following command:\n", tp)
-		content += "\n"
-		content += "[source,shell]\n"
-		content += "----\n"
-		content += fmt.Sprintf("kubectl apply -f %s-binding.yaml\n", k.Name)
-		content += "----\n"
-
-	}
-
-	content += "// THIS FILE IS AUTOMATICALLY GENERATED: DO NOT EDIT\n"
-
 	if _, err := os.Stat(docFile); err == nil {
 		err = os.Remove(docFile)
 		handleGeneralError(fmt.Sprintf("cannot remove file %q", docFile), err)
